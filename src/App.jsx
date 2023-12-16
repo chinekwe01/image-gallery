5bff2debcf54edcb14a04529bece4698882b073a
clever_chinekwe <chinekwe_clever@outlook.com>
2023-12-16 14:42:34 +0100

    created react app with tailwindcss and added an image to app component

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="max-w-sm rounded overflow-hidden">
+      <img src="https://source.unsplash.com/random" alt="" />
+    </div>
+  )
+}
+
+export default App
